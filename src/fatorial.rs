febf7fc9aada88142196fdea3cd85ccc6602960d
bbonassi <bbonassi@lliege.com.br>
2026-02-25 16:13:06 -0300

    recursao

--- src/fatorial.rs
+++ src/fatorial.rs
@@ -5,10 +5,23 @@ fn fatorial(mut number:i32) -> i32 {
     number
 }
 
+fn recurcao(fatorial:i32) -> i32 {
+    let mut result = fatorial;
+    let mut i = 2;
+
+    while result > 1 {
+        result = result / i;
+        i+=1
+    }
+    i-1
+}
+
 pub fn fatorial_recurcao() {
     let number:i32 = 4;
 
-    let result:i32 = fatorial(number);
+    let fatorial:i32 = fatorial(number);
+    let recurcao:i32 = recurcao(fatorial);
     
-    println!("{}", result);
+    println!("Fatorial: {}", fatorial);
+    println!("Recurcao: {}", recurcao);
 }
